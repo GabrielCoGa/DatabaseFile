ad221d77e39b19658acc825d4bd8dcc4533b4645
GabrielCoGa <gabriel.cortes@hotmail.com>
2025-11-20 16:34:00 +0100

    sigo trabajando en el fichero listdatabase.py

--- Listdatabase.py
+++ Listdatabase.py
@@ -1,30 +1,33 @@
 #https://youtu.be/4yEKWer4cVI?si=hZPuBqtvZYlgMv8p
 """
-Conceerned with storin and retrieving books from a list
+Concerned with storing and retrieving books from a csv file.
+Format of the csv file:
+name,author,read\n
 """
 
-books =[]
+books_file = 'boot.txt'
 
 def add_book(name, author):
-    books.append({'name': name, 'author': author, 'read': False})
+    with open(books_file, 'a') as file:
+        file.write(f'{name},{author}'0) #Cero for false, one for true
 
 def get_all_books():
+    #[[name, author,read], [name, author,read]]
+    with open(books_file, 'r') as file:
+        lines = [line.strip().split(',') for line in file.readlines()]
+    
+    """books = [
+        {'name': line[0], 'author': line[1], 'read': line[2]}
+        for line in lines
+    ]
     return books
+    """
+    return [
+        {'name': line[0], 'author': line[1], 'read': line[2]}
+        for line in lines
+    ]
 
 def mark_book_as_read(name):
-    for book in books:
-        if book['name'] == name:
-            book['read'] = True
-
-"""def delete_book(name):
-    for book in books:
-        if book['name'] == name:
-            books.remove(book)
-            
- Esto esta considerado una mala tractica porque la lista queda inconsistente           
- """
+    pass
 def delete_book(name):
-    global books #Estamos haciendo referencia a la lista books de inicio que es global, 
-                 #esto es por el ambito(scope) de las variables, que dentro de la funcion
-                 #serian si no locales
-    books = [book for book in books if book['name'] != name]
+   pass
